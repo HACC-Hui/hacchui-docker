2621170896242740f0b7cd0b5752c165cabe2938
Cam Moore <cammymoore07@gmail.com>
2020-10-03 10:37:12 -1000

    Trying to set the slackbot environment variables

--- docker-compose-run.sh
+++ docker-compose-run.sh
@@ -5,12 +5,16 @@
 # substitutes them into the docker-compose.yml configuration file.
 # See: https://docs.docker.com/compose/compose-file/#variable-substitution
 
-# Source and export NGINX_SERVER_NAME for Nginx and View services in docker-compose.yml. Passed on to View as ROOT_URL.
+# Source and export NGINX_SERVER_NAME for Nginx and HACC-Hui services in docker-compose.yml. Passed on to View as ROOT_URL.
 . ./config/nginx/nginx.env
 export NGINX_SERVER_NAME
 
-# View Config Environment Var
+# HACCHui Config Environment Var
 export METEOR_SETTINGS=$(cat ./config/settings.production.json)
+
+export SLACK_SIGNING_SECRET=$(cat ./config/slackbot.signing.secret.txt)
+
+export SLACK_BOT_TOKEN=$(cat ./config/slackbot.token.txt)
 
 # Startup Docker-Compose. Note: Be sure that docker-compose.yml is same directory as this script.
 docker-compose up -d --remove-orphans
